8261a9c3a9b5eb5d9f034fdf8507a5edcc8b423a
DavLin <davlin@creatorise.com>
2021-09-16 21:47:44 +0200

    return the string

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ module.exports = ensure;
 
 function ensure(object) {
     console.log('ensure ~ object', object);
+
+    return {
+        is: {
+            string: () => {
+                return object;
+            },
+        },
+    };
 }
